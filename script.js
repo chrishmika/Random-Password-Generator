37fd147ba9782460c77e822ff2a4344a656fbd39
Shehan Chrishmika <shehanchrishmika123@gmail.com>
2024-06-14 12:55:43 +0530

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -35,9 +35,53 @@ const capitalsChar = [
   "&",
 ];
 
-const charCount = capitalsChar.length;
-console.log(charCount);
+function generatePsw() {
+  var charLengthGiven = Number(document.getElementsByClassName("passwordLength")[0].value);
+  const values = document.getElementsByClassName("passwordOut");
+  const charVarient = document.getElementsByName("charVarient");
+  const charCount = capitalsChar.length;
+  var newPassword = "";
+  //   for (const item of values) {
+  //     item.value = "";
+  //   }
 
-console.log(capitalsChar[Math.floor(Math.random() * 100) % charCount]);
-console.log(capitalsChar[Math.floor(Math.random() * 100) % charCount].toLowerCase());
-console.log(Math.floor(Math.random() * 100) % 26);
+  if (charLengthGiven > 25 || charLengthGiven < 2) {
+    for (const item of values) {
+      item.value = "Max Lenght is 2 - 25 !!!";
+    }
+  } else {
+    charVarient.forEach((item) => {
+      if (item.checked && item.value == "useSimple") {
+        for (const item of values) {
+          while (charLengthGiven > 0) {
+            newPassword = newPassword + capitalsChar[Math.floor(Math.random() * 100) % charCount].toLowerCase();
+            charLengthGiven = charLengthGiven - 1;
+          }
+          item.value = newPassword;
+        }
+        console.log(item.value);
+      } else if (item.checked && item.value == "useCapital") {
+        for (const item of values) {
+          while (charLengthGiven > 0) {
+            newPassword = newPassword + capitalsChar[Math.floor(Math.random() * 100) % charCount];
+            charLengthGiven = charLengthGiven - 1;
+          }
+          item.value = newPassword;
+        }
+        console.log(item.value);
+      } else if (item.checked && item.value == "useMix") {
+        for (const item of values) {
+          item.value = "";
+        }
+        console.log(item.value);
+      }
+    });
+  }
+}
+
+// const charCount = capitalsChar.length;
+// console.log(charCount);
+
+// console.log(capitalsChar[Math.floor(Math.random() * 100) % charCount]);
+// console.log(capitalsChar[Math.floor(Math.random() * 100) % charCount].toLowerCase());
+// console.log(Math.floor(Math.random() * 100) % 26);
